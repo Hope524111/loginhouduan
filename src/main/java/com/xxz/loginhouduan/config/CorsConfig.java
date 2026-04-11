ef56795b2e53072cc2e7c48c8ddce85fc880121e
Developer <dev@example.com>
2026-04-11 18:24:39 +1000

    Fix CORS config syntax

--- src/main/java/com/xxz/loginhouduan/config/CorsConfig.java
+++ src/main/java/com/xxz/loginhouduan/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://hooopex.com", "https://www.hooopex.com",)
+                .allowedOrigins("https://hooopex.com", "https://www.hooopex.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
